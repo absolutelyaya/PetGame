2d41e9003f0be5908d742b9b9ae61a80d0eb9171
yaya-nyan <Florian.Denker@live.de>
2021-07-24 20:23:57 +0200

    added more stats

--- Assets/Scripts/Pets/PetStats.cs
+++ Assets/Scripts/Pets/PetStats.cs
@@ -9,4 +9,9 @@ public class PetStats
     public float Speed = 6f;
     public Vector2 MoveInterval = new Vector2(1.5f, 3f);
     public Vector2 SpeakInterval = new Vector2(45f, 60f);
+    public int SpeakSpeed = 16;
+    //Hunger
+    public float StomachSize = 10f;
+    public float HungerRate = 0.1f;
+    public float HungerThreshold = 1f;
 }
