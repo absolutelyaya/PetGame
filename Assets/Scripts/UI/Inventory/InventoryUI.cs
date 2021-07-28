48e708a2364c5f60e2509e4e23b1ce75f33ae92b
yaya-nyan <Florian.Denker@live.de>
2021-07-28 18:42:17 +0200

    Inventory Tabs now use Labels instead of icons

--- Assets/Scripts/UI/Inventory/InventoryUI.cs
+++ Assets/Scripts/UI/Inventory/InventoryUI.cs
@@ -21,6 +21,8 @@ public class InventoryUI : MonoBehaviour
     public enum InventoryCategory
     {
         None,
-        Food
+        Food,
+        Placeholder1,
+        Placeholder2
     }
 }
